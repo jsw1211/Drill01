d1dfd91dcce64eef5f723b6322f77ad5b19524fc
jsw1211 <jswtiger@naver.com>
2023-09-15 12:13:03 +0900

    s_move
    s누르면 아래쪽방향으로 50만큼 가는 함수

--- wasd_move_turtle.py
+++ wasd_move_turtle.py
@@ -12,3 +12,7 @@ def a_move_turtle():
     turtle.stamp()
     turtle.setheading(180)
     turtle.forward(50)
+def s_move_turtle():
+    turtle.stamp()
+    turtle.setheading(270)
+    turtle.forward(50)
