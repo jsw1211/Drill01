3d236d880ecbd85dedd44a80093c23d6c0227836
jsw1211 <jswtiger@naver.com>
2023-09-15 12:11:47 +0900

    w_move
    w누르면 위쪽방향으로 50만큼 가는 함수

--- wasd_move_turtle.py
+++ wasd_move_turtle.py
@@ -4,3 +4,7 @@ def d_move_turtle():
     turtle.stamp()
     turtle.setheading(0)
     turtle.forward(50)
+def w_move_turtle():
+    turtle.stamp()
+    turtle.setheading(90)
+    turtle.forward(50)
